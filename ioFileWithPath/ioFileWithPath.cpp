310693c3f5a1c543e8cde93d2117fe13160aeb92
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-06-04 18:25:57 +0700

    add include int main memanggil array elemen ke 5

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -1,22 +1,18 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int main() {
 	string baris;
-	string NamaFile;
 
-	cout << "Masukan Nama File : ";
-	cin >> NamaFile;
-
-	// membuka file dalam mode menulis
+	// membuka file dalam mode menulis.
 	ofstream outfile;
-	//menunjuk ke sebuah nama file
-	outfile.open(NamaFile + ".txt", ios::out);
+	// menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
 	//unlimited loop untuk menulis
 	while (true) {
 		cout << "- ";
@@ -27,11 +23,28 @@ int main() {
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
-	//selesai dalam menulis sekarang tutup filenya
+	//selesai dakam memnulis sekarang tutup filenya
 	outfile.close();
 
-	//Membuka file dalam mode membaca
+	//membuka file dalam mode membaca
 	ifstream infile;
 	//menunjuk ke sebuah file
-	infile.open(NamaFile + ".txt", ios::in);
+	infile.open("contohfile.txt");
 
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file tidak ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tamppilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+}
