769127f9874837344af88570bb3331ea7a242331
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-06-04 17:29:16 +0700

    add outfile and ifstream and infile

--- iofaile/iofaile.cpp
+++ iofaile/iofaile.cpp
@@ -23,4 +23,12 @@ int main() {
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//Membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
 
